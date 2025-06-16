c93d3432ddbedb77af7a215efe3192ec33830841
Augusto <augusto.k.ogawa@gmail.com>
2025-06-16 12:02:35 -0300

    fix: type enum

--- src/main/java/com/example/RAS_api/dto/auth/RegistroUsuarioDto.java
+++ src/main/java/com/example/RAS_api/dto/auth/RegistroUsuarioDto.java
@@ -2,6 +2,9 @@ package com.example.RAS_api.dto.auth;
 
 import com.example.RAS_api.entity.EnumRole;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -24,6 +27,8 @@ public class RegistroUsuarioDto {
     @NotBlank @Size(min = 6, max = 40)
     private String senha;
 
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
     private EnumRole role;
 
 }
